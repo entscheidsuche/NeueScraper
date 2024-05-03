94ae0274fae374178ea2ed6382705b272e956cfd
Jörn Erbguth <joern@erbguth.net>
2024-05-03 04:14:52 +0200

    Update BL_Gerichte.py

--- NeueScraper/spiders/BL_Gerichte.py
+++ NeueScraper/spiders/BL_Gerichte.py
@@ -16,21 +16,38 @@ class BL_Gerichte(BasisSpider):
 	name = 'BL_Gerichte'
 
 	URLs={
-		"Kantonsgericht": { "url": "/politik-und-behorden/gerichte/rechtsprechung/kantonsgericht/chronologische-anordnung", "direkt": False},
+		"Kantonsgericht": { "url": "/politik-und-behorden/gerichte/rechtsprechung/kantonsgericht_rs/chronologische-anordnung", "direkt": False},
 		"Steuergericht": { "url": "/politik-und-behorden/gerichte/rechtsprechung/steuergericht", "direkt": False},
 		"Enteignungsgericht": { "url": "/politik-und-behorden/gerichte/rechtsprechung/enteignungsgericht/entscheide-chronologisch", "direkt": True},
 		"Zwangsmassnahmengericht": { "url": "/politik-und-behorden/gerichte/rechtsprechung/zwangsmassnahmengericht", "direkt": False}}
 	HOST="https://www.baselland.ch"
+	PROXY="http://v2202109132150164038.luckysrv.de:8181/"
 	
 	reJahre=re.compile(r'<a\s[^>]*href="(?P<URL>[^"]+)"[^>]*>\s*(?P<Jahr>(?:20|19)\d\d)\s*</a>')
+	
+	HEADER={
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+		'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'DNT': '1',
+		'Connection': 'keep-alive',
+		'Upgrade-Insecure-Requests': '1',
+		'Sec-Fetch-Dest': 'document',
+		'Sec-Fetch-Mode': 'navigate',
+		'Sec-Fetch-Site': 'none',
+		'Sec-Fetch-User': '?1',
+		'Pragma': 'no-cache',
+		'Cache-Control': 'no-cache'
+	}
 	
 	def request_generator(self):
 		requests=[]
 		for r in self.URLs:
 			if self.URLs[r]['direkt']:
-				request = scrapy.Request(url=self.HOST+self.URLs[r]['url'], callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': r})			
+				request = scrapy.Request(url=self.PROXY+self.HOST+self.URLs[r]['url'], callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': r})			
 			else:
-				request = scrapy.Request(url=self.HOST+self.URLs[r]['url'], callback=self.parse_jahresliste, errback=self.errback_httpbin, meta={'Gericht': r})
+				request = scrapy.Request(url=self.PROXY+self.HOST+self.URLs[r]['url'], callback=self.parse_jahresliste, errback=self.errback_httpbin, meta={'Gericht': r})
 			requests.append(request)
 		return requests
 	
@@ -52,8 +69,8 @@ class BL_Gerichte(BasisSpider):
 			logger.info(str(len(jahre))+" Jahrgänge gefunden für "+response.meta['Gericht'])
 		for j in self.reJahre.finditer(antwort):
 			if self.ab is None or int(j.group('Jahr'))>=int(self.ab):
-				logger.info("Hole Jahr "+j.group('Jahr')+" für "+response.meta['Gericht'])
-				request = scrapy.Request(url=j.group('URL'), callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht']})
+				logger.info("Hole Jahr "+j.group('Jahr')+" für "+response.meta['Gericht']+" mit URL:"+j.group('URL'))
+				request = scrapy.Request(url=self.PROXY+j.group('URL'), callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht']})
 				yield request			
 
 	def parse_trefferliste(self, response):
@@ -66,7 +83,7 @@ class BL_Gerichte(BasisSpider):
 			andere_monate=response.xpath(".//a[contains(translate(.,'\xa0',' '),' bis ')]/@href")
 			for l in andere_monate:
 				logger.info("Andere Monate-URL: "+l.get())
-				request = scrapy.Request(url=l.get(), callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht'], 'Teiljahr': True})
+				request = scrapy.Request(url=self.PROXY+l.get(), callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht'], 'Teiljahr': True})
 				yield request
 		urteile=response.xpath("//table/tbody/tr[td[1]//a]")
 		if len(urteile)==0:
@@ -95,19 +112,19 @@ class BL_Gerichte(BasisSpider):
 						else:
 							url+="/@@download/file/"+url.split("/")[-1]
 							
-						item['PDFUrls']=[url]
+						item['PDFUrls']=[self.PROXY+url]
 							
 						if self.check_blockliste(item):
 							logger.info("PDF-Item: "+json.dumps(item))
 							yield item
 					else:
-						item['HTMLUrls']=[url]
+						item['HTMLUrls']=[self.PROXY+url]
 						item['Num']=url.split("/")[-1]
 						if len(item['Num'])<6:
 							item['Num']=url.split("/")[-2]+"/"+item['Num']
 						item['Signatur'], item['Gericht'], item['Kammer']=self.detect(item['VGericht'],"",item['Num'])
 						if self.check_blockliste(item):
-							request = scrapy.Request(url=url, callback=self.parse_document, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht'], 'item': item})
+							request = scrapy.Request(url=self.PROXY+url, callback=self.parse_document, errback=self.errback_httpbin, meta={'Gericht': response.meta['Gericht'], 'item': item})
 							logger.info("HTML-Item bis jetzt: "+json.dumps(item))
 							yield request
 				else:
