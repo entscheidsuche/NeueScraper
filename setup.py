36b565c534a2588b820200d98f8aba0a08e0fcc7
Jörn Erbguth <joern@erbguth.net>
2025-07-24 05:05:10 +0200

    Anpassung Version, damit Deployment fehlerfrei funktioniert

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'project',
+    name         = 'entscheidsuche.ch Scraper',
     version      = '1.0',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = NeueScraper.settings']},
 )
+
+
