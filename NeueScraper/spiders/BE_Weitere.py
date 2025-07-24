d325a45386385c5447b0bd54cfb5ebc045c499d0
Jörn Erbguth <joern@erbguth.net>
2025-07-24 05:13:08 +0200

    Drei weitere Beschwerdeentscheide für Bern

--- NeueScraper/spiders/BE_Weitere.py
+++ NeueScraper/spiders/BE_Weitere.py
@@ -14,26 +14,25 @@ class BE_Weitere(BasisSpider):
 	name = 'BE_Weitere'
 
 	HOST ="https://www.gef.be.ch"
-	suchseiten={ "BE_VB_002": ['https://www.erz.be.ch','/erz/de/index/direktion/organisation/generalsekretariat/rechtsdienst_dererziehungsdirektion/Beschwerdeentscheide.html','table'],
-				"BE_VB_003": ['https://www.gef.be.ch','/gef/de/index/direktion/organisation/ra/RechtsprechungGEF.html','table'],
-				"BE_VB_004": ['https://www.jgk.be.ch','/jgk/de/index/direktion/organisation/gba/entscheide/grundbuchrecht_im_engeren_sinne.html','list1'],
-				"BE_VB_005": ['https://www.jgk.be.ch','/jgk/de/index/direktion/organisation/gba/entscheide/grundbuchgebuehren.html','list1'],
-				"BE_VB_006": ['https://www.jgk.be.ch','/jgk/de/index/direktion/organisation/gba/entscheide/handaenderungssteuern.html','list1'],
-				"BE_NAB_001": ['https://www.jgk.be.ch','/jgk/de/index/aufsicht/notariat/Entscheide/Administrativentscheide.html','list2'],
-				"BE_NAB_002": ['https://www.jgk.be.ch','/jgk/de/index/aufsicht/notariat/Entscheide/Moderationsentscheide.html','list2'],
-				"BE_NAB_003": ['https://www.jgk.be.ch','/jgk/de/index/aufsicht/notariat/Entscheide/Disziplinarentscheide.html','list1']}
-		
+	suchseiten={ "BE_VB_002": ['https://www.bkd.be.ch','https://www.bkd.be.ch/de/start/ueber-uns/die-organisation/bkd-generalsekretariat/rechtsdienst-bkd/ausgewaehlte-beschwerdeentscheide.html','table'],
+				"BE_VB_003": ['https://www.gsi.be.ch','https://www.gsi.be.ch/de/start/ueber-uns/generalsekretariat/rechtsabteilung/rechtsprechung.html','table'],
+				"BE_VB_004": ['https://www.gba.dij.be.ch/content','https://ktbe.jaxforms.com/formservice/services/rest/generic/json?handler=jaxGenericDataPoolHandler&language=de&paging=false&id=77e7438e-f9c8-4e73-9302-bbfec0ad04b3&displayID=standard','list1']}
+
 	reMeta=re.compile(r"(?P<art>[^\s]+)\s(?P<num>[A-Z0-9\.\-\s]+)\svom\s(?P<datum>\d+\.\s(?:"+"|".join(BasisSpider.MONATEde)+")\s(?:19|20)\d\d)")
 	reMetaOhne=re.compile(r"(?P<art>.+)\svom\s(?P<datum>\d+\.\s(?:"+"|".join(BasisSpider.MONATEde)+")\s(?:19|20)\d\d)")
 	reMetaZusatz=re.compile(r"vgl\.\s(?P<art>[^\s]+)\s(?P<num>[A-Z0-9\.\- ]+)\svom\s(?P<datum>\d+\.\s(?:"+"|".join(BasisSpider.MONATEde)+")\s(?:19|20)\d\d)")
 	
+	
+# https://ktbe.jaxforms.com/formservice/services/rest/generic/json?handler=jaxGenericDataPoolHandler&language=de&paging=false&id=77e7438e-f9c8-4e73-9302-bbfec0ad04b3&displayID=standard
+# https://ktbe.jaxforms.com/formservice/services/rest/generic/json;jsessionid=A6A98D460DC0A3B70AB4B29CB17AF10C.4000patc2?handler=jaxGenericDataPoolHandler&language=de&paging=false&id=77e7438e-f9c8-4e73-9302-bbfec0ad04b3&displayID=standard
+# https://www.gba.dij.be.ch/content/dam/gba_dij/dokumente/de/entscheide/Beschwerdeentscheid%202021.DIJ.4477%2022.09.2022.pdf
 	
 	def request_generator(self):
 		""" Generates scrapy frist request
 		"""
 		request_liste=[]
 		for g in self.suchseiten:
-			request_liste.append(scrapy.Request(url=self.suchseiten[g][0]+self.suchseiten[g][1], callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'signatur': g, 'host': self.suchseiten[g][0], 'typ': self.suchseiten[g][2]}))		
+			request_liste.append(scrapy.Request(url=self.suchseiten[g][1], callback=self.parse_trefferliste, errback=self.errback_httpbin, meta={'signatur': g, 'host': self.suchseiten[g][0], 'typ': self.suchseiten[g][2]}))		
 		return request_liste
 
 	def __init__(self, ab=None, neu=None):
@@ -45,7 +44,7 @@ class BE_Weitere(BasisSpider):
 
 	def parse_trefferliste(self, response):
 		logger.info("parse_trefferliste response.status "+str(response.status)+" URL:"+response.request.url)
-		antwort=response.body_as_unicode()
+		antwort=response.text
 		logger.info("parse_trefferliste Rohergebnis "+str(len(antwort))+" Zeichen")
 		logger.info("parse_trefferliste Rohergebnis: "+antwort[:10000])
 		
@@ -53,58 +52,40 @@ class BE_Weitere(BasisSpider):
 		host=response.meta['host']
 		typ=response.meta['typ']
 		if typ=="table":
-			entscheide=response.xpath("//table[@cellspacing='0' and thead]/tbody/tr")
+			entscheide=response.xpath("//div[@data-testid='table']/table[@cellspacing='1']/tbody/tr[th[not(@scope='col')] and not(th[2])]")
 		elif typ=="list1":
-			entscheide=response.xpath("//div[@class='textBild floatingComponent section']/ul/li")
+			struktur=json.loads(antwort)
+			entscheide=struktur['results']
 		else:
-			entscheide=response.xpath("//div[@class='linkliste parsys section']/ul/li")
+			logger.error("Unbekannter typ: "+typ+", Host: "+host)
 
 		trefferzahl=len(entscheide)
 		logger.info(str(trefferzahl)+" Entscheide für "+signatur)
 		for e in entscheide:
 			item={}
-			text=e.get()
 			if typ=="table":
-				edatum=PH.NC(e.xpath("./td[1]/text()").get(), error="Kein E-Datum gefunden in ("+signatur+"): "+text)
+				text=e.get()
+				logger.info("Treffertext: "+text)
+				edatum=PH.NC(e.xpath("string(./th[1])").get(), error="Kein E-Datum gefunden in ("+signatur+"): "+text)
 				item['EDatum']=self.norm_datum(edatum)
-				item['Num']=PH.NC(e.xpath("./td[2]/a/text()").get(),error="Keine Geschäftsnummer in ("+signatur+"): "+text)
-				url=PH.NC(e.xpath("./td[2]/a/@href").get(), error="Keine URL zu PDF-Dokument in ("+signatur+"): "+text)
-				item['Titel']=PH.NC(e.xpath("./td[3]//text()").get(), warning="kein Titel in ("+signatur+"): "+text)
-				
-			else:
-				if typ=="list1":
-					meta=PH.NC(e.xpath("./strong/text()").get(), warning="Metastring nicht gefunden in ("+signatur+"): "+text)
-					item['Titel']=PH.NC(e.xpath(".//a/text()").get(), warning="kein Titel in ("+signatur+"): "+text)
-				else:
-					meta=PH.NC(e.xpath(".//h3/text()").get(), warning="Metastring nicht gefunden in ("+signatur+"): "+text)
-					item['Titel']=PH.NC(e.xpath("./span[@class='info linkinfo']/text()[1]").get(), warning="Titel nicht gefunden in ("+signatur+"): "+text)
-				metamatch=self.reMeta.match(meta)
-				if metamatch:
-					item['Num']=PH.NC(metamatch.group('num'), error="Keine Geschäftsnummer in meta: "+meta+", in ("+signatur+"): "+text)
-					item['EDatum']=self.norm_datum(PH.NC(metamatch.group('datum'), error="Kein Datum in meta: "+meta+", in ("+signatur+"): "+text))
-					item['Entscheidart']=PH.NC(metamatch.group('art'), warning="Keine Entscheidart in meta: "+meta+", in ("+signatur+"): "+text)
-				else:
-					zusatz=e.xpath("./text()")
-					metamatchohne=self.reMetaOhne.search(meta)
-					if metamatchohne is None:
-						logger.error("Metastring '"+meta+"' matched nicht, und metastring matched auch nicht einfachen Regex in ("+signatur+") für: "+text)
-					else:
-						item['EDatum']=self.norm_datum(PH.NC(metamatchohne.group('datum'), error="Kein Datum in meta (ohne): "+meta+", in ("+signatur+"): "+text))
-						item['Entscheidart']=PH.NC(metamatchohne.group('art'), warning="Keine Entscheidart in meta (ohne): "+meta+", in ("+signatur+"): "+text)
-						if len(zusatz)>1:
-							zusatztext=zusatz[len(zusatz)-1].get()
-							zusatzmatch=self.reMetaZusatz.search(zusatztext)
-							if zusatzmatch:
-								item['Num']=PH.NC(zusatzmatch.group('num'), error="Keine Geschäftsnummer in zusatz: "+zusatztext+", in ("+signatur+"): "+text)
-							else:
-								logger.warning("Metastring '"+meta+"' matched nicht, Zusatz vorhanden, aber zusatzstring '"+zusatztext+"' matched auch nicht in ("+signatur+") für: "+text)
-								item['Num']=''
-						else:
-							logger.warning("Metastring nicht gegen regexgematched: '"+meta+"' und kein Zusatzstring in ("+signatur+"): "+text)
-							item['Num']=''
-				url=PH.NC(e.xpath(".//a/@href").get(), error="Keine URL zu PDF-Dokument in ("+signatur+"): "+text)
+				item['Num']=PH.NC(e.xpath("normalize-space(string(./td[1]//a[1]))").get(),error="Keine Geschäftsnummer in ("+signatur+"): "+text)
+				url=PH.NC(e.xpath("./td[1]//a[1]/@href").get(), error="Keine URL zu PDF-Dokument in ("+signatur+"): "+text)
+				item['Titel']=PH.NC(e.xpath("normalize-space(string(./td[2]//text()))").get(), warning="kein Titel in ("+signatur+"): "+text)
+			elif typ=="list1":
+				text=json.dumps(e)
+				logger.info("Treffertext: "+text)
+				url=PH.NC(host+e['DateiName'], error="Keine URL zu PDF-Dokument in ("+signatur+"): "+text)
+				edatum=PH.NC(e['Datum'], error="Kein E-Datum gefunden in ("+signatur+"): "+text)
+				item['EDatum']=self.norm_datum(edatum)
+				item['Num']=PH.NC(e['EntscheidNr'],error="Keine Geschäftsnummer in ("+signatur+"): "+text)
+				item['Rechtsgebiet']=e['Thema_de']
+				item['Abstract_de']=e['Kurzbeschrieb_de']
+				item['Abstract_fr']=e['Kurzbeschrieb_fr']
 
-			item['PDFUrls']=[host+url]
+			if url[:4]=="http":
+				item['PDFUrls']=[url]
+			else:
+				item['PDFUrls']=[host+url]
 			item['Signatur']=signatur
 			item['Gericht'], item['Kammer']=self.detect_by_signatur(signatur)
 			logger.info("Item gelesen: "+json.dumps(item))
