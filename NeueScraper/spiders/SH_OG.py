48e6e16d753486d269db3ac09d4762427d4dec30
Jörn Erbguth <joern@erbguth.net>
2025-08-29 03:58:25 +0200

    Update SH_OG

--- NeueScraper/spiders/SH_OG.py
+++ NeueScraper/spiders/SH_OG.py
@@ -8,6 +8,7 @@ from scrapy.http.cookies import CookieJar
 import datetime
 from NeueScraper.spiders.basis import BasisSpider
 from NeueScraper.pipelines import PipelineHelper as PH
+import codecs
 
 logger = logging.getLogger(__name__)
 
@@ -18,27 +19,21 @@ class SH_OG(BasisSpider):
     }
 	name = 'SH_OG'
 
-	START_URL='/CMS/json_list.jsp?filter_customposttypeid_int=10300&listtype=repository&status=published&filter_approvedpaths_string=*%2F8527%2F9005%2F*&rows=-1'
 	HOST ="https://obergerichtsentscheide.sh.ch"
-	COOKIE_URL="/CMS/Webseite/Obergerichtsentscheide-2272928-DE.html"
-	STUB_URL="/CMS/json_list.jsp?filter_generic_parent_repository_string="
-	STUB_A_URL="/CMS/lists/list.jsp?rows=100&sort=sortable_datetime+desc&filter_customposttypeid_int=402&filter_published_string=published&filter_approvedpaths_string=*%2F8527%2F9005%2F2272923%2F2272926%2F"
-	STUB_B_URL="%2F*&filter_text=*&filter_language_string=DE&status=&start=0&kioskid=066df47d-46e9-72e0-5902-aa4d05b81cbc&mode=portrait&slider=false&language=DE&colclass=false&domainpath=%2F8527%2F9005%2F&kiosktype=kioskWidget"
-	STUB_DOC_A="/CMS/content.jsp?contentid="
-	STUB_DOC_B="&language=DE"
-	STUB_FILE_A="/CMS/lists/list.jsp?rows=1&filter_contentid_int="
-	STUB_FILE_B="&status=published&start=0&kioskid=41d82fe3-73d3-15cb-7560-9ba0da22dd84&mode=portrait&slider=false&language=DE&colclass=false&domainpath=%2F8527%2F9005%2F&kiosktype=singleContent"
-	GET_FILE="/CMS/get/file/"
-	reTreffer=re.compile(r' contentid="(?P<contentid>[^"]+)"')
-	rePDFID=re.compile(r'"contentid":"(?P<pdfid>[^"]+)"')
-	reFilename=re.compile(r'\\"fileName\\":\\"(?P<filename>[^\\]+)\\"')
-	reDatum=re.compile(r'[0-9B] vom (?P<datum>\d+\.\s*(?:'+r'|'.join(BasisSpider.MONATEde)+r')\s(?:19|20)\d\d)<')
-	reLeerzeile=re.compile(r'<p class="post_text">&nbsp;</p>')
-	reUmbruch=re.compile(r'(<br ?/?>)*\s*(</p>\s+<p class="post_text">\s*)+(<br ?/?>)*')
-	reAnfang=re.compile(r'^Nr\.\s[0-9\/]+\s.{1,4}<br>')
+	START_CONTENT_ID="2272928"
+	START_URL="/CMS/Webseite/Obergerichtsentscheide-{content_id}-DE.html"
+	LIST_URL="/CMS/lists/list?rows=300&sort=sortable_datetime%20desc&filter_customposttypeid_int=402&filter_published_string=published&filter_approvedpaths_string=*%2F8527%2F9005%2F2272923%2F2272926%2F{content_id}%2F*&rows=300&filter_text=()&filter_language_string=DE&status=&start=0&mode=portrait&slider=false&language=DE&colclass=false&domainpath=%2F8527%2F9005%2F&kiosktype=kioskWidget"
+	DOC_URL="/CMS/Webseite/Obergerichtsentscheide/{jahr}-{content_id}-DE.html"
+	DOC_PDF_URL="/CMS/lists/list?rows=1&filter_contentid_int={content_id}&status=published&start=0&mode=portrait&slider=false&language=DE&colclass=false&domainpath=%2F8527%2F9005%2F&kiosktype=singleContent"
+	
+	reData=re.compile(r'window\.CONTENT_DATA\s*=\s*(?P<data>\{.*?\})\s*;')
+	reTag=re.compile(r'<[^>]+>')
+	reDocData=re.compile(r'var\s+wrapper\s+=\s+\$\(\'\#(?P<id>[0-9]+)_(?P<gid>(?:[0-9a-f-]+|null))\'\);\s+try\s+\{\s+var\s+kacheldata\s*=\s*JSON\.parse\("(?P<data>(?:\\.|[^"\\])*)"\);', re.DOTALL | re.MULTILINE | re.VERBOSE)
+	reNumPre=re.compile(r'^Nr\.\s+')
+	
 	
 	def get_next_request(self):
-		request=scrapy.Request(url=self.HOST+self.COOKIE_URL, callback=self.parse_step0, errback=self.errback_httpbin)
+		request=scrapy.Request(url=self.HOST+self.START_URL.format(content_id=self.START_CONTENT_ID), callback=self.parse_jahre, errback=self.errback_httpbin,meta={"content_ids":[self.START_CONTENT_ID],"typ":"Jahre"})
 		return request
 	
 	def __init__(self, neu=None):
@@ -46,127 +41,147 @@ class SH_OG(BasisSpider):
 		self.neu=neu
 		self.request_gen = [self.get_next_request()]
 
-	def parse_step0(self, response):
-		logger.debug("parse_step1 response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		logger.info("parse_step1 Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_step1 Rohergebnis: "+antwort[:30000])
-		request=scrapy.Request(url=self.HOST+self.START_URL, callback=self.parse_step1, errback=self.errback_httpbin)
-		yield request
-		
+	def parse_jahre(self, response):
+		typ=response.meta["typ"]
+		logger.info(f"parse_jahre {typ} url {response.url}, response.status {response.status}")
+		antwort=response.text
+		logger.info("parse_jahre "+typ+" Rohergebnis "+str(len(antwort))+" Zeichen")
+		logger.info("parse_jahre "+typ+" Rohergebnis: "+antwort[:30000])
+		content_ids=response.meta["content_ids"]
+		script=response.xpath("//script[not(@*)][1]/text()")
+		if script:
+			scripttext=script.get()
+			datamatch=self.reData.search(scripttext)
+			if datamatch:
+				data=datamatch.group("data")
+				datastruct=json.loads(data)
+				logger.info(f"parse_jahre {typ}: {len(datastruct)} Werte.")
+				for entry in datastruct.values():
+					logger.info(f"parse {typ}: contenttypeid {entry['contenttypeid']}")
+					if entry['contenttypeid']=='499' or entry['contenttypeid']=='402':
+						content_id=entry['data_repository_id']
+						logger.info(f"content_id {content_id}")
+						if content_id in content_ids:
+							logger.info("parse_jahre "+typ+": Den Eintrag selbst gefunden.")
+						else:
+							jahr=entry["data_kachellabel"]
+							data_overview_image=json.loads(entry['data_overview_image'])
+							logger.info("parse_jahre "+typ+": gefunden: "+data_overview_image['text'])
+							url=self.HOST+self.LIST_URL.format(content_id=content_id)
+							request=scrapy.Request(url=url, callback=self.parse_liste, errback=self.errback_httpbin, meta={"content_ids": content_ids+[content_id],"jahr":jahr, "typ":"Liste"})
+							yield request
 
-	def parse_step1(self, response):
-		logger.debug("parse_step1 response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		logger.info("parse_step1 Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_step1 Rohergebnis: "+antwort[:30000])
-		struktur=json.loads(antwort)
-		weiter=struktur[0]['contentid']
-		logger.info("ID (step1): "+weiter)
-		request=scrapy.Request(url=self.HOST+self.STUB_URL+weiter, callback=self.parse_step2, errback=self.errback_httpbin)
-		yield request
-		
-	def parse_step2(self, response):
-		logger.debug("parse_step2 response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		logger.info("parse_step2 Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_step2 Rohergebnis: "+antwort[:30000])
-		struktur=json.loads(antwort)
-		weiter=struktur[0]['contentid']
-		logger.info("ID (step2): "+weiter)
-		request=scrapy.Request(url=self.HOST+self.STUB_URL+weiter, callback=self.parse_step3, errback=self.errback_httpbin)
-		yield request
-		
-	def parse_step3(self, response):
-		logger.debug("parse_step3 response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		logger.info("parse_step3 Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_step3 Rohergebnis: "+antwort[:30000])
-		struktur=json.loads(antwort)
-		for r in struktur:
-			weiter=r['contentid']
-			jahr=r['label']
-			url=self.HOST+self.STUB_A_URL+weiter+self.STUB_B_URL
-			logger.info("ID (step3): "+weiter+" Jahr "+jahr+" ("+url+")")
-			request=scrapy.Request(url=url, callback=self.parse_jahr, errback=self.errback_httpbin, meta={'jahr': jahr})
-			yield request
-		
-	def parse_jahr(self, response):
-		logger.debug("parse_jahr response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		jahr=response.meta['jahr']
-		logger.info("parse_jahr "+jahr+": "+response.url+" Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_jahr Rohergebnis: "+antwort[:30000])
-		
-		treffer=self.reTreffer.findall(antwort)
-		zahl=len(treffer)
-		if zahl==100: logger.error("100 Treffer in "+jahr+", weitere Treffer gehen ggf. verloren")
-		else: logger.info(str(zahl)+" Treffer gefunden")
-		for t in treffer:
-			logger.info("parse_jahr "+jahr+" Contentid: "+t)
-			url=self.HOST+self.STUB_DOC_A+t+self.STUB_DOC_B
-			logger.info("ID (parse_jahr): "+t+" Jahr "+jahr+" ("+url+")")
-			request=scrapy.Request(url=url, callback=self.parse_doc, errback=self.errback_httpbin, meta={'jahr': jahr, 'doc': t})
-			yield request
+
+	def parse_liste(self, response):
+		typ=response.meta["typ"]
+		jahr=response.meta["jahr"]
+		logger.info(f"parse_liste {typ} ({jahr}), url {response.url}, response.status {response.status}")
+		antwort=response.text
+		logger.info("parse_liste "+typ+" Rohergebnis "+str(len(antwort))+" Zeichen")
+		logger.info("parse_liste "+typ+" Rohergebnis: "+antwort[:30000])
+		content_ids=response.meta["content_ids"]
+		for m in self.reDocData.finditer(antwort):
+			json_literal = m.group('data')          # der reine JSON-Text
+			json_literal_clean=json_literal.replace("\\\\\"","\'")
+			json_literal_clean=codecs.decode(json_literal_clean, "unicode_escape")
+			json_literal_clean=json_literal_clean.replace("\\\"","\"")
+			json_literal_clean=json_literal_clean.replace("&nbsp;"," ")
 			
+			content_id=m.group('id')
+			content_gid=m.group('gid')
+			logger.info(f"parse_liste: content_id:{content_id}, content_gid:{content_gid}, json:{json_literal}, json_clean:{json_literal_clean}")
+			try:
+				obj = json.loads(json_literal_clean)      # jetzt echtes Python-Dict
+				item={}
+				item['DocID']=content_gid
+				titel=obj['text']
+				item['Titel']=self.reTag.sub("",titel)
+				num=obj['title']
+				item['Num']=self.reNumPre.sub("",num)
+				logger.info(f"Dokument {item['Num']}: {item['Titel']}")
+				url=self.HOST+self.DOC_URL.format(jahr=jahr, content_id=content_id)
+				request=scrapy.Request(url=url, callback=self.parse_doc, errback=self.errback_httpbin, meta={"content_ids": content_ids+[content_id],"jahr":jahr, "typ":"Doc", "item":item, "titel":titel})
+				yield request
+								
+			except json.JSONDecodeError as err:
+				logger.error(f'Fehler beim JSON-Parse: {err}')
+		
 	def parse_doc(self, response):
-		logger.debug("parse_doc response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		jahr=response.meta['jahr']
-		doc=response.meta['doc']
-		logger.info("parse_doc "+jahr+": "+response.url+" Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_doc Rohergebnis: "+antwort[:30000])
-
-		struktur=json.loads(antwort)
-		num=PH.NC(struktur['data_kachellabel'],error="kein AZ"+antwort)
-		if num[0:4]=="Nr. ": num=num[4:]
-		item={}
-		item['Num']=num
-		pdatum=PH.NC(struktur['data_publication_date'],warning="kein Publikationsdatum in "+antwort)		
-		item['PDatum']=self.norm_datum(pdatum, warning="Kein P-Datum identifiziert in "+pdatum)
-		titel=PH.NC(struktur['data_listlabel'], warning="kein data_listlabel (Leitsatz) in "+antwort)
-		titel2=self.reAnfang.sub("",titel)
-		logger.info("Titel von "+titel+" zu "+titel2)
-		item['Titel']=titel2
-		abstract=PH.NC(struktur['data_post_content'], warning="kein data_post_content (Titel) in "+antwort)
-		edatummatch=self.reDatum.search(abstract)
-		if edatummatch:
-			edatum=edatummatch.group('datum')
-			logger.info("EDatum gefunden: "+edatum)
-			item['EDatum']=self.norm_datum(edatum, warning="Kein E-Datum identifiziert in "+edatum)
-		abstract=abstract.replace('<p class="post_text">&nbsp;</p>','') # Leerzeilen rauswerfen
-		abstract=self.reUmbruch.sub("<br>",abstract) #Umbrüche durch br ersetzen
-		abstract=abstract.replace('<p class="post_text">','') #einzelne Tags noch löschen
-		abstract=abstract.replace('</p>','')
-		
-		item['Abstract']=abstract
-		item['VGericht']=PH.NC(struktur['data_custom_author'], warning="kein data_custom_author (VGericht) in "+antwort)
-		item['DocID']=doc
-		pdfidstring=PH.NC(struktur['data_widget_data'], error="keine ID für das PDF gefunden in "+antwort)
-		pdfidmatch=self.rePDFID.search(pdfidstring)
-		if pdfidmatch:
-			pdfid=pdfidmatch.group("pdfid")
-			url=self.HOST+self.STUB_FILE_A+pdfid+self.STUB_FILE_B
-			logger.info("ID (parse_doc): "+doc+" Jahr "+jahr+" ("+url+")")			
-			request=scrapy.Request(url=url, callback=self.parse_file, errback=self.errback_httpbin, meta={'jahr': jahr, 'doc': doc, 'item': item})
-			yield request
-		else:
-			logger.error("kein Match für pdfid in "+pdfidstring)
-		
-	def parse_file(self, response):
-		logger.debug("parse_file response.status "+str(response.status))
-		antwort=response.body_as_unicode()
-		jahr=response.meta['jahr']
-		doc=response.meta['doc']
+		typ=response.meta["typ"]
+		logger.info(f"parse_doc {typ} url {response.url}, response.status {response.status}")
+		antwort=response.text
+		logger.info("parse_doc "+typ+" Rohergebnis "+str(len(antwort))+" Zeichen")
+		logger.info("parse_doc "+typ+" Rohergebnis: "+antwort[:30000])
+		content_ids=response.meta["content_ids"]
 		item=response.meta['item']
-		logger.info("parse_file "+doc+": "+response.url+" Rohergebnis "+str(len(antwort))+" Zeichen")
-		logger.info("parse_file Rohergebnis: "+antwort[:30000])
-		
-		filename=self.reFilename.search(antwort)
-		if filename:
-			item['PDFUrls']=[self.HOST+self.GET_FILE+filename.group('filename')]
-			item['Signatur'], item['Gericht'], item['Kammer'] = self.detect(item['VGericht'],"",item['Num'])
-			logger.info("Entscheid: "+json.dumps(item))
-			yield item
-		else:
-			logger.error("Konnte keine PDF-Datei für "+doc+" aus "+jahr+" finden.")
+		script=response.xpath("//script[not(@*)][1]/text()")
+		jahr=response.meta['jahr']
+		if script:
+			scripttext=script.get()
+			datamatch=self.reData.search(scripttext)
+			if datamatch:
+				data=datamatch.group("data")
+				logger.info(f"parse_doc data: {data}")
+				datastruct=json.loads(data)
+				pdf_id=""
+				for entry in datastruct.values():
+					logger.info(f"parse_doc {typ}: contenttypeid {entry['contenttypeid']}")
+					if entry['contenttypeid']=='402':
+						leitsatz=entry["data_post_content"]
+						leitsatz=leitsatz.replace("&lt;","<")
+						leitsatz=leitsatz.replace("&gt;",">")						
+						item["Leitsatz"]=self.reTag.sub("",leitsatz)
+						item["PDatum"]=PH.NC(self.norm_datum(entry["data_publication_date"]),warning=item["Num"]+" hat kein PDatum in "+json.dumps(entry))
+						pdf_id_string=PH.NC(entry['data_widget_data'],warning=item["Num"]+" hat kein data_widget_data in "+json.dumps(entry))
+						pdf_id_struct=json.loads(pdf_id_string)
+						if "contentid" in pdf_id_string:
+							pdf_id=PH.NC(pdf_id_struct[0]["cols"][1]["fields"][0]["data"]["contentid"],error="keine Informationen zur id des requests für die PDF/filemeta für "+item["Num"]+" in "+pdf_id_string)
+						else:
+							logger.warning("Kein PDF für "+item["Num"])
+					elif entry['contenttypeid']=='101':
+						item["EDatum"]=PH.NC(self.norm_datum(entry["data_custom_publication_date_date"]),warning=item["Num"]+" hat kein PDatum in "+json.dumps(entry))					
+						filemeta_string=PH.NC(entry["data_filemeta"],error="keine Informationen zu PDF/filemeta für "+item["Num"]+" in "+json.dumps(entry))
+						filemeta=json.loads(filemeta_string)
+						url=self.HOST+PH.NC(filemeta["url"],error="keine Informationen zur ULR in PDF/filemeta für "+item["Num"]+" in "+json.dumps(entry))
+						item["PDFUrls"]=[url]
+				if "PDFUrls" not in item:
+					if pdf_id:
+						url=self.HOST+self.DOC_PDF_URL.format(content_id=pdf_id)
+						request=scrapy.Request(url=url, callback=self.parse_doc_pdf, errback=self.errback_httpbin, meta={"content_ids": content_ids,"jahr":jahr, "typ":"Doc_Pdf", "item":item})
+						yield request
+					else:
+						if "Leitsatz" in item and "Titel" in item:
+							item["html"]=response.meta["titel"]+leitsatz
+							item["HTMLUrls"]=[response.url]
+						else:
+							logger.error("kein PDF und kein HTML text für Dokument "+item["Num"]+" gefunden.")
+
+						logger.error("kann keinen Weg zum PDF finden für "+item["Num"]+" in "+json.dumps(entry))
+				else:
+					item['Signatur'], item['Gericht'], item['Kammer'] = self.detect("","",item["Num"])
+					logger.info(f"parse_doc Item: {json.dumps(item)}")
+					yield item
+					
+	def parse_doc_pdf(self, response):
+		typ=response.meta["typ"]
+		logger.info(f"parse_doc_pdf {typ} url {response.url}, response.status {response.status}")
+		antwort=response.text
+		logger.info("parse_doc_pdf "+typ+" Rohergebnis "+str(len(antwort))+" Zeichen")
+		logger.info("parse_doc_pdf "+typ+" Rohergebnis: "+antwort[:30000])
+		content_ids=response.meta["content_ids"]
+		item=response.meta['item']
+		script=response.xpath("//script[not(@*)][1]/text()")
+		if script:
+			scripttext=script.get()
+			datamatch=self.reData.search(scripttext)
+			if datamatch:
+				data=datamatch.group("data")
+				logger.info(f"parse_doc data: {data}")
+				filemeta=json.loads(data)
+				url=self.HOST+PH.NC(filemeta["url"],error="keine Informationen zur ULR in PDF/filemeta für "+item["Num"]+" in "+data)
+				item["PDFUrls"]=[url]
+				item['Signatur'], item['Gericht'], item['Kammer'] = self.detect("","",item["Num"])
+				logger.info(f"parse_doc Item: {json.dumps(item)}")
+				yield item
+
+
