f89731e213034f7a3aaa79fcc95f52f28a33b3ff
Jörn Erbguth <joern@erbguth.net>
2025-08-29 03:58:46 +0200

    Update UR_Gerichte.py

--- NeueScraper/spiders/UR_Gerichte.py
+++ NeueScraper/spiders/UR_Gerichte.py
@@ -18,8 +18,8 @@ class UR_Gerichte(BasisSpider):
 	SUCH_URL='/rechtsprechung?search_publikation_filter[searchText]='
 	HOST ="https://www.ur.ch"
 
-	reURL=re.compile(r'^<a href="(?P<URL>[^"]+)">(?P<Num>\d+(?:_[A-Z]+|/\d+)\s+(?:[A-Z]+\s+)?(?:\d+\s+)?[^\.;]+)(?:\.|;)\s+(?P<Titel>[^<]+)?(?:</a>)?$')
-	reOhneURL=re.compile(r'^(?P<Num>\d+(?:_[A-Z]+|/\d+)\s+(?:[A-Z]+\s+)?(?:\d+\s+)?[^\.;]+)(?:\.|;)\s+(?P<Titel>.+)$')
+	reURL=re.compile(r'^<a (?:[a-z]+="[^"]+"\s+)*href="(?P<URL>[^"]+)"(?:\s+[a-z]+="[^"]+")*>(?:<span class="sr-only">)?(?P<Num>\d+(?:_[A-Z]+|/\d+)\s+(?:[A-Z]+\s+)?(?:\d+\s+)?[^\.;]*)(?:\.|;)?\s+(?P<Titel>[^<]+)?(?:</span>Download)?(?:</a>)?$')
+	reOhneURL=re.compile(r'^(?P<Num>\d+(?:_[A-Z]+|/\d+)\s+(?:[A-Z]+\s+)?(?:\d+\s+)?[^\.;]*)(?:\.|;)?\s+(?P<Titel>.+)$')
 	reNurURL=re.compile(r'^<a href="(?P<URL>[^"]+)"')
 	
 	def get_next_request(self):
@@ -34,7 +34,7 @@ class UR_Gerichte(BasisSpider):
 
 	def parse_trefferliste(self, response):
 		logger.debug("parse_trefferliste response.status "+str(response.status))
-		antwort=response.body_as_unicode()
+		antwort=response.text
 		logger.info("parse_trefferliste Rohergebnis "+str(len(antwort))+" Zeichen")
 		logger.debug("parse_trefferliste Rohergebnis: "+antwort[:30000])
 
@@ -53,14 +53,19 @@ class UR_Gerichte(BasisSpider):
 				if "_downloadBtn" in entscheid and entscheid['_downloadBtn']:
 					linkstring=entscheid['_downloadBtn']
 					logger.info("Entscheid "+meta+" mit Linkstring: "+linkstring)
-					linkmatch=self.reNurURL.search(linkstring)
+					linkmatch=self.reURL.search(linkstring)
 					if linkmatch:
 						url=linkmatch['URL']
+						metamatch=linkmatch
 					else:
-						logger.error("URL nicht erkannt in: "+linkstring)
-					metamatch=self.reOhneURL.search(meta)
-					if not(metamatch):
-						logger.error("kein Metamatch bei vorhandenem Linkstring für "+meta+" ("+meta.encode('utf-8').hex()+")")
+						linkmatch=self.reNurURL.search(linkstring)
+						if linkmatch:
+							url=linkmatch['URL']
+						else:
+							logger.error("URL nicht erkannt in: "+linkstring)
+						metamatch=self.reOhneURL.search(meta)
+						if not(metamatch):
+							logger.error("kein Metamatch bei vorhandenem Linkstring für "+meta+" ("+meta.encode('utf-8').hex()+")")
 				else:
 					logger.info("Entscheid "+meta+" ohne Linkstring")
 					metamatch=self.reURL.search(meta)
@@ -74,16 +79,16 @@ class UR_Gerichte(BasisSpider):
 					if entscheid['herausgeber']:
 						item['Titel']+=" ("+entscheid['herausgeber']+")"
 					url=url.strip()
-					if url[-4:].lower()=='.pdf':
-						item['PDFUrls']=[self.HOST+url]
-						item['Signatur'], item['Gericht'], item['Kammer'] = self.detect("","",item['Num'])
-						logger.info("PDF-Entscheid direkt: "+json.dumps(item))
-						logger.info("Hole nun URL als PDF: "+self.HOST+url)
-						yield item
-					else:
-						logger.info("Hole nun URL als HTML: "+self.HOST+url)
-						request=scrapy.Request(url=self.HOST+url,callback=self.parse_details, errback=self.errback_httpbin, meta={'item': item})
-						yield request
+#					if url[-4:].lower()=='.pdf':
+					item['PDFUrls']=[self.HOST+url]
+					item['Signatur'], item['Gericht'], item['Kammer'] = self.detect("","",item['Num'])
+					logger.info("PDF-Entscheid direkt: "+json.dumps(item))
+					logger.info("Hole nun URL als PDF: "+self.HOST+url)
+					yield item
+#					else:
+#						logger.info("Hole nun URL als HTML: "+self.HOST+url)
+#						request=scrapy.Request(url=self.HOST+url,callback=self.parse_details, errback=self.errback_httpbin, meta={'item': item})
+#						yield request
 				else:
 					logger.error("kein Metamatch für "+meta)
 				
@@ -101,7 +106,7 @@ class UR_Gerichte(BasisSpider):
 			yield item
 			
 		else:
-			antwort=response.body_as_unicode()
+			antwort=response.text
 			logger.info("parse_details Rohergebnis "+str(len(antwort))+" Zeichen")
 			logger.debug("parse_details Rohergebnis: "+antwort[:30000])
 
@@ -113,4 +118,4 @@ class UR_Gerichte(BasisSpider):
 				logger.info("Entscheid: "+json.dumps(item))
 				yield item
 
-			
+
